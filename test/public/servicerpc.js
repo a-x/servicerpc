be9e86b431a633a55ab7dc0ff84d1a031e33e70d
a_x <a-x@ukr.net>
2013-01-20 18:12:01 -0500

    socjjs & html service

--- test/public/servicerpc.js
+++ test/public/servicerpc.js
@@ -3,13 +3,13 @@
 
     var validate = function(req) {
         var error;
-        if (typeof(req.method) !== 'function') {
+        if (typeof(req.method) !== 'string') {
             error = {
                 code: 32600,
                 message: 'Invalid Request'
             };
         }
-        else if (req.params && typeof(req.method) !== 'object') {
+        else if (req.params && typeof(req.params) !== 'object') {
             error = {
                 code: 32602,
                 message: 'Invalid params'
@@ -25,17 +25,25 @@
 
     };
     
-    var Trasport = function (args) {
+    var inherits = function(child, parent) {
+        var F = function() { };
+        F.prototype = parent.prototype;
+        child.prototype = new F();
+        child.prototype.constructor = child;
+        child.superclass = parent.prototype;
+        child.parent = parent;
     };
-    Trasport.prototype.send = function(req, callback) {
-    };
-
-    var Sock = function(args) {
-        this._args = args || {};
+    
+    var Transport = function (url) {
+        this._args = {url: url};
         this._callbacks = {};
         this._init();
     };
-    Sock.prototype._clearCallbacks = function() {
+
+    Transport.prototype.send = function(req, callback) {
+    };
+
+    Transport.prototype._clearCallbacks = function() {
         for(var id in this.callbacks) {
             if (!this._callbacks.hasOwnProperty(id))
                 continue;
@@ -46,6 +54,12 @@
         }
         this._callbacks = {}
     }
+
+    var Sock = function(url) {
+        Transport.apply(this, arguments);
+    };
+
+    inherits(Sock, Transport);
     
     Sock.prototype._init = function() {
         this._clearCallbacks();
@@ -53,7 +67,7 @@
         this.sockjs = new SockJS(sockjs_url);
         var self = this;
         this.sockjs.onopen    = function()  {
-            self.opened = true;
+            self._opened = true;
         };
         this.sockjs.onmessage = function(e) {
             var data = e.data || '',
@@ -64,7 +78,6 @@
             } catch (err) {
                 error = { code:-32603, message: 'Internal error' };
             }
-    
             res = (typeof(res) === 'object') ? res : {error: error || { code:-32603, message: 'Internal error' }};
               
             var callback = res.id && self._callbacks[res.id];
@@ -72,36 +85,58 @@
             
         };
         this.sockjs.onclose   = function()  {
-            self.opened = false;
+            self._opened = false;
             self._init();
         };
         this.sockjs.onerror   = function(e)  {
-            self.opened = false;
+            self._opened = false;
             self._init();
             alert(e);
         };
     };
     
     Sock.prototype.send = function(req, callback) {
+//        !this._opened && callback && (callback({error: error || { code:-32603, message: 'Internal error' }));
         callback && (this._callbacks[req.id] = callback);
         this.sockjs.send(JSON.stringify(req));
     }
     
-    var Service = module.exports = function(args) {
-        this.callbacks = {}
-        this.url = args.url || '';
-        this.transport = new Sock(args);
+    var Http= function(url) {
+        Transport.apply(this, arguments);
+    };
+
+    inherits(Http, Transport);
+    
+    Http.prototype._init = function() {
+        this._clearCallbacks();
+    };
+    
+    Http.prototype.send = function(req, callback) {
+        callback && (this._callbacks[req.id] = callback);
+        $.getJSON(this._args.url || '/', req)
+            .success(function(res) { 
+                callback && callback(res);
+            })
+            .error(function() { 
+                callback && callback({error: { code:-32603, message: 'Internal error' }});
+            })
+    }
+    
+    var Service = module.Service = function(url) {
+        this._url = url || '';
+//        this._transport = new Sock(this._url);
+        this._transport = new Http(this._url);
     }
 
     Service.prototype.call = function(req, callback) {
         var err = validate(req);
         if (err) {
             callback && callback(err);
-        }
+        } 
         else {
             callback ? (req.id = uuid()) : (delete(req.id));
             req.jsonrpc = '2.0';
-            this.transport.send(req, function(res) {
+            this._transport.send(req, function(res) {
                 res = res || {};
                 var error = res.error,
                     result = res.result;
@@ -111,97 +146,5 @@
 
 
     }
-    url: "",
-    _counter: 0 // TODO: UUID ?
-    ,
-    applyFunctions: function() {
-        this.call("", 0, $.proxy(function(data) {
-            console.log(data)
-            if (!data) return;
-
-            for (var key in data) {
-                this[key] = (function(name) {
-                    return function(d, c) {
-                        this.call(name, d, c)
-                    }
-                })(key)
-            }
-        }, this))
-    },
-    call: function(name, data, callback) {
-        // set data object
-        data = {
-            "id": "" + (this._counter++) + "-" + (+new Date),
-            "jsonrpc": "2.0",
-            "method": name,
-            "params": data
-        };
-        var options = {
-            "url": this.url,
-            "type": "POST",
-            "dataType": 'json',
-            "data": data
-        };
-        if (callback) {
-            options.success = $.proxy(function(data, textStatus, jqXHR) {
-                data && callback(data.result);
-            }, this);
-            options.error = $.proxy(function(jqXHR, textStatus, errorThrown) {
-                callback(undefined, {
-                    "code": -1,
-                    "message": textStatus
-                });
-            }, this);
-        }
-        $.ajax(options);
-    },
-},
-
-function(data) {
-    data && $.extend(this, data);
-});
-
-//------------------------------------------------------------------------------
-
-exports.SocketService = util.extender({
-    url: "",
-    _response: "response",
-    _request: "request",
-    _counter: 0, // TODO: UUID ?
-    _callbacks: {},
-    call: function(name, data, callback) {
-        // set data object
-        data = {
-            id: "" + (this._counter++) + "-" + (+new Date),
-            name: name,
-            data: data
-        };
-        console.log(data.id);
-        this.socket.emit(this._request, data);
-        if (callback) this._callbacks[data.id] = callback;
-    },
-    response: function(data) {
-        var id = data.id;
-        console.log(id);
-        var callback = this._callbacks[id];
-        if (callback) {
-            delete this._callbacks[id];
-            callback(data.data);
-        }
-        else {
-            console.log("Not callback for id: " + id);
-        }
-    }
-},
-
-function(data) {
-    data && $.extend(this, data);
-
-    this.socket = io.connect(window.location.host + this.url);
-    this.socket.on(this._response, $.proxy(this.response, this));
-});
-
-//------------------------------------------------------------------------------
-
-})();
+})( typeof(module) !== 'undefined' && module.exports ? module : this );
 /**   MODULE  **/
