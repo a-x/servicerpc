a4e7dd956b28333deb0c3f0d8d1eb21256671166
a_x <a-x@ukr.net>
2013-02-06 16:14:09 -0500

    all transport

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 module.exports = require('./lib/jsonrpc2-connect');
+module.exports.Connect = require('./lib/jsonrpc2-connect');
+module.exports.SockJS = require('./lib/jsonrpc2-sockjs');
+module.exports.WebSocket = require('./lib/jsonrpc2-ws');
