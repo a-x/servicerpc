09b9b8008fce2595993242ad31377773d78e23fc
a_x <a-x@ukr.net>
2013-02-08 12:21:16 -0500

    socket.io

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = require('./lib/jsonrpc2-connect');
 module.exports.Connect = require('./lib/jsonrpc2-connect');
 module.exports.SockJS = require('./lib/jsonrpc2-sockjs');
+module.exports.SocketIO = require('./lib/jsonrpc2-socket.io');
 module.exports.WebSocket = require('./lib/jsonrpc2-ws');
